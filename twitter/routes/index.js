8acbec1e20491ac150783a92ab6bde441b04f92a
Harrison Cole <harrisonccole@gmail.com>
2015-10-07 18:02:15 -0700

    Correct login now directs to main.hbs

--- twitter/routes/index.js
+++ twitter/routes/index.js
@@ -4,7 +4,17 @@ var db = require("../database.js");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var user = db.get("users", "Tom");
-  res.render('index', { title: user.bio });
+  res.render('login', { title: user.bio });
 });
+router.post('/', function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+  var user = db.get('users', username);
+  if (username === user.username && password === user.password) {
+    res.render('main', {username: username});
+  } else {
+    res.render('login', {error: 'Unable to find user'});
+  }
+})
 
 module.exports = router;
